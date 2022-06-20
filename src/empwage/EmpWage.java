ab029e6301fdf8109f84989a2811aa800a3f8a67
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-21 00:35:55 +0530

    Adding Part Time Employee Wage

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -6,15 +6,22 @@ public class EmpWage {
 		System.out.println("Welcome to Employee Wage");
 		// TODO Auto-generated method stub
 		int employeePresent = 1;
+		int withPartTimeWage = 2;
 		int wagePerHour = 20;
 		int numberOfHours = 8;
-		int isPresent = (int) Math.floor(Math.random() * 10) % 2;
+		int partTimeWage = 4;
+		int isPresent = (int) Math.floor(Math.random() * 10) % 3;
 		if (isPresent == employeePresent) {
 			System.out.println("Employee is Prsent");
 			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-		} else {
+			}
+		else if (isPresent == withPartTimeWage) {
+			System.out.println("Employee is Present");
+			System.out.println("Employee earn with PartTime = " +numberOfHours * (withPartTimeWage+wagePerHour));
+			}
+		else {
 			System.out.println("Employee is not Present");
 			System.out.println("Employee earn = 0");
-		}
+			}
 	}
 }
