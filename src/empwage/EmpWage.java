e34872615cae7fd71692bc42ca9a4017926ef25f
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-20 20:47:42 +0530

    Welcome message

---
+++ src/empwage/EmpWage.java
@@ -0,0 +1,11 @@
+package empwage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage");
+		// TODO Auto-generated method stub
+
+	}
+
+}
