36b4e8e1358a6d5559b2dba89d68a08a7ff28860
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-22 04:15:59 +0530

    Calculating total working hrs wage

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -1,38 +1,37 @@
 package empwage;
 
 public class EmpWage {
+	public static final int employeePresent = 1;
+	public static final int withPartTimeWage = 2;
+	public static final int wagePerHour = 20;
+	public static final int totalWorkingHours =100;
+	public static final int totalWorkingDays = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
 		// TODO Auto-generated method stub
-		int employeePresent = 1;
-		int withPartTimeWage = 2;
-		int wagePerHour = 20;
-		int numberOfHours = 8;
-		int partTimeHours = 4;
-		int workingDays = 20;
-		int isPresent = (int) Math.floor(Math.random() * 10) % 3;
+		int numberOfHours = 0;
+		int empWagePerDay = 0;
+		int totalWorkDays = 0 , totalWorkHours = 0;
+		int totalEmployeeWage =0;
+		while (totalWorkHours<= totalWorkingHours && totalWorkDays < totalWorkingDays) {
+		totalWorkHours++;
+		totalWorkDays++;
+		int isPresent = (int) Math.floor(Math.random()*10) %3;
 		switch (isPresent) {
-		case 1:
-			System.out.println("Employee is Prsent");
-			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-			System.out.println("Employee Monthly Earn = " + (numberOfHours * wagePerHour) * workingDays);
-			break;
-		case 2:
-			System.out.println("Employee is Present");
-			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-			System.out.println("Employee earn with PartTime= " + partTimeHours * wagePerHour);
-			System.out.println("Employee earn Whole day = " + (numberOfHours + partTimeHours) * wagePerHour);
-			System.out.println(
-					"Employee Monthly Earn = " + ((numberOfHours + partTimeHours) * wagePerHour) * workingDays);
-			break;
-		case 0:
-			System.out.println("Employee is not Present");
-			System.out.println("Employee earn = 0");
-			break;
-		default:
-			System.out.println("Invalid");
-			break;
+			case employeePresent :
+				numberOfHours =8;
+				break;
+			case withPartTimeWage :
+				numberOfHours =4;
+				break;
+			default :
+				numberOfHours = 0;
 		}
+		totalWorkHours += numberOfHours;
+		System.out.println("day  = " + totalWorkDays + " No. of Hours = " +numberOfHours);
+		}
+		totalEmployeeWage= totalWorkHours * wagePerHour;
+		System.out.println("Total Employee Wage is "+ totalEmployeeWage);
 	}
 }
