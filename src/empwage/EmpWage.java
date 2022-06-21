223c97b28e673eb44f7e67f8ef4772ebeb02ad71
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-21 16:37:41 +0530

    Calculating Monthly wage

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -9,16 +9,22 @@ public class EmpWage {
 		int withPartTimeWage = 2;
 		int wagePerHour = 20;
 		int numberOfHours = 8;
-		int partTimeWage = 4;
+		int partTimeHours = 4;
+		int workingDays = 20;
 		int isPresent = (int) Math.floor(Math.random() * 10) % 3;
 		switch (isPresent) {
 		case 0:
 			System.out.println("Employee is Prsent");
 			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+			System.out.println("Employee Monthly Earn = " + (numberOfHours * wagePerHour) * workingDays);
 			break;
 		case 1:
 			System.out.println("Employee is Present");
-			System.out.println("Employee earn with PartTime = " +numberOfHours * (withPartTimeWage+wagePerHour));
+			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+			System.out.println("Employee earn with PartTime= " + partTimeHours * wagePerHour);
+			System.out.println("Employee earn Whole day = " + (numberOfHours + partTimeHours) * wagePerHour);
+			System.out.println(
+					"Employee Monthly Earn = " + ((numberOfHours + partTimeHours) * wagePerHour) * workingDays);
 			break;
 		case 2:
 			System.out.println("Employee is not Present");
