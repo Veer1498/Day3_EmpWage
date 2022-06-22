2b53e47dd2299b1355314804c238c8b304ade486
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-22 23:58:50 +0530

    Calculating Wages For Total Working Hours

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -6,27 +6,34 @@ public class EmpWage {
 	public static final int wagePerHour = 20;
 	public static final int totalWorkingHours =100;
 	public static final int totalWorkingDays = 20;
-
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
 		// TODO Auto-generated method stub
-		int numberOfHours = 0;
-		int empWagePerDay = 0;
+		int numberOfHours = 0 ;
+		int empWagePerday;
 		int totalWorkDays = 0 , totalWorkHours = 0;
 		int totalEmployeeWage =0;
 		while (totalWorkHours<= totalWorkingHours && totalWorkDays < totalWorkingDays) {
-		totalWorkHours++;
 		totalWorkDays++;
 		int isPresent = (int) Math.floor(Math.random()*10) %3;
 		switch (isPresent) {
 			case employeePresent :
 				numberOfHours =8;
+				totalWorkHours += wagePerHour; 
+				empWagePerday = (numberOfHours * wagePerHour);
+				System.out.println("Employee is Present, Daily Wage = " + empWagePerday);
 				break;
 			case withPartTimeWage :
 				numberOfHours =4;
+				totalWorkHours += wagePerHour; 
+				empWagePerday = (numberOfHours * wagePerHour);
+				System.out.println("Employee is Part Time Present, Daily Wage = " + empWagePerday);
 				break;
 			default :
 				numberOfHours = 0;
+				totalWorkHours += wagePerHour; 
+				empWagePerday = (numberOfHours * wagePerHour);
+				System.out.println("Employee is Absent , Daily Wage = " + empWagePerday);
 		}
 		totalWorkHours += numberOfHours;
 		System.out.println("day  = " + totalWorkDays + " No. of Hours = " +numberOfHours);
